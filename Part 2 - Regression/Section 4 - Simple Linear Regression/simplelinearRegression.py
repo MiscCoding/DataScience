2f89691a7096979aa6b9731b013fd7ea99ad1b6b
neogeoss <neogeo-s@hanmail.net>
2020-02-09 16:01:50 +0900

    Preprocessing of simple linear regression
    preprocessing of a linear regression

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simplelinearRegression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simplelinearRegression.py
@@ -8,8 +8,8 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 1].values
 
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
