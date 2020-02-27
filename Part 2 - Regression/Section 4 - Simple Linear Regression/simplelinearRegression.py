5414398d320f0adf42500ac4a64cf354a535bcf8
neogeoss <neogeo-s@hanmail.net>
2020-02-27 21:27:27 +0900

    Salary vs experience test graph plot added

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simplelinearRegression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simplelinearRegression.py
@@ -22,10 +22,18 @@ regressor.fit(X_train, y_train)
 #predicting the test set results
 y_pred = regressor.predict(X_test)
 
-#plotting the prediction data
+#plotting the training set results
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'Blue')
 plt.title("Salary VS Experience {Training set }")
+plt.xlabel("Years of experience")
+plt.ylabel("Salary")
+plt.show()
+
+#plotting the test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'Blue')
+plt.title("Salary VS Experience {Test set }")
 plt.xlabel("Years of experience")
 plt.ylabel("Salary")
 plt.show()
