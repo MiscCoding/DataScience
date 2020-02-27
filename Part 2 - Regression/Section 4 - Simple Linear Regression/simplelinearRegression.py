903dfc2057b0d509acb16a594b5f165533c15aeb
neogeoss <neogeo-s@hanmail.net>
2020-02-27 11:22:38 +0900

    predicting the salary based on the test set

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simplelinearRegression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simplelinearRegression.py
@@ -10,6 +10,14 @@ dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
-
-from sklearn.cross_validation import train_test_split
+#Splitting the dataset abd test set
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+#Fitting simple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#predicting the test set results
+y_pred = regressor.predict(X_test)
