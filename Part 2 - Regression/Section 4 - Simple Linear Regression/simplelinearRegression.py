a680be360a74d83a84ac23650740d6241b9ad7fa
neogeoss <neogeo-s@hanmail.net>
2020-02-27 21:19:44 +0900

    Plotting graphs

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simplelinearRegression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simplelinearRegression.py
@@ -21,3 +21,11 @@ regressor.fit(X_train, y_train)
 
 #predicting the test set results
 y_pred = regressor.predict(X_test)
+
+#plotting the prediction data
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'Blue')
+plt.title("Salary VS Experience {Training set }")
+plt.xlabel("Years of experience")
+plt.ylabel("Salary")
+plt.show()
