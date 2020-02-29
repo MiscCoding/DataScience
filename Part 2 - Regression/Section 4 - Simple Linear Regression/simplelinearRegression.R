37aab4004a9d80fc319678a724ebec1fda5f6107
neogeoss <neogeo-s@hanmail.net>
2020-03-01 00:11:14 +0900

    Test and training set graphs plot

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simplelinearRegression.R
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simplelinearRegression.R
@@ -29,4 +29,15 @@ library(ggplot2)
 ggplot() + 
   geom_point(aes(x = training_set$YearsExperience,y=training_set$Salary), 
              colour = 'red') +
-  geom_line(aes(x = training_set$YearsExperience, y = y_pred))
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') + 
+  ggtitle('Salary vs Experience (Training set)') + xlab('Years of experience') + ylab('Salary')
+
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = test_set$YearsExperience,y=test_set$Salary), 
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') + 
+  ggtitle('Salary vs Experience (test set)') + xlab('Years of experience') + ylab('Salary')
+
