3f533866c4a8a4dd8674f5398aa97e2636808472
neogeoss <neogeo-s@hanmail.net>
2020-03-08 00:05:05 +0900

    predicting test results

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multilinearRegression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multilinearRegression.py
@@ -33,4 +33,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 #fitting multiple linear regression to the training set.
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit()
+regressor.fit(X_train, y_train)
+
+#predicting the testset results
+
