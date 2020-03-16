359b5b40994b03ad88e4e4c5fe607b1640f510df
neogeoss <neogeo-s@hanmail.net>
2020-03-17 00:25:42 +0900

    Backward elimination

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multilinearRegression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multilinearRegression.py
@@ -36,4 +36,17 @@ regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 #predicting the testset results
+y_pred = regressor.predict(X_test)
 
+#Building the optional model using backward elimination
+import statsmodels.formula.api as sm
+X = np.append(arr =np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0,1,2,3,4,5]]
+
+regressor_OLS = sm.OLS(endog = y, exog= X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,1,3,4,5]]
+
+regressor_OLS = sm.OLS(endog = y, exog= X_opt).fit()
+regressor_OLS.summary()
